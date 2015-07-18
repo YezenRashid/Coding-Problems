a7f9acec12518b2ee670e1fff67e84c63b8d882c
YezenRashid <yezenrashid@hotmail.com>
2015-07-18 15:35:18 -0700

    zuneBug

---
+++ zuneBug/zuneBug.java
@@ -0,0 +1,15 @@
+int days = getTotalDaysSince1980();
+int year = 1980;
+while (days > 365) {  // subtract out years
+    if (isLeapYear(year)) {
+        if (days > 366) {
+            days -= 366;
+            year += 1;
+        } else {
+            break;
+        }
+    } else {
+        days -= 365;
+        year += 1;
+    }
+}
